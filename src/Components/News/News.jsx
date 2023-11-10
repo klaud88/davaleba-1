ed505d6dd8856404125797f342559ed525ec2277
klaud88 <80270123+klaud88@users.noreply.github.com>
2023-11-10 17:40:02 +0400

    deploy test

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,41 +1,27 @@
 import React, { useState } from 'react';
 import './Style/News.css';
-import Posts from './Posts';
+
 
 
 
 const News = () => {
 
+    const [lengths, setLength] = useState('');
+    
+    const handle = (x) => {
+    const length = x.target.value;
+    setLength(length);
 
-    const articles = [
-        {
-            id: 1,
-            author: 'გიორგი',
-            date: "2.11.2023"
-
-        }, 
-        {
-            id: 2,
-            author: 'გუგა',
-            date: "28.10.2023"
-
-        },
-        {
-            id: 3,
-            author: 'დათა',
-            date: "29.10.2023"
-
-        },
-     ];
-
-   
+  if (length.length > 10){
+     alert("მაქსიმალური სიმბოლოების რაოდენობა არის 10");
+     setLength("");
+      }//რამზეს ასე სწორია? else, თუ არ მინდა საერთოდ, რომ არ დავწერო?
+    }   
+    
     return (
 
         <>
-    <ul>
-        {articles.map(news => <li key={news.id}>
-        <Posts  id={news.id} author={news.author} date={news.date}/></li>)}
-    </ul>
+        <input style={{width: 200,height: 30,margin: 30,padding: 5}} value={lengths} onChange={handle}/>
         </>
     )
 }
